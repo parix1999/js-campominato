49eb559f78777e63914d98e17c8fa2c36af6183b
parix1999 <parisottoedoardo@gmail.com>
2021-06-10 16:47:12 +0200

    js-5.0

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ function randomNumber (min,max) {
     return Math.floor(Math.random() * (max - min + 1)+ min);
 }
 
-var numeriComputer = randomNumber(1,100);
+
 listComputerNum = [];
-while (listComputerNum.lenght <= 16){
+while (listComputerNum.lengh < 16){
+    var numeriComputer = randomNumber(1,100);
+    
     if (!numeriComputer.includes(listComputerNum)){
         var numbersComputer = numeriComputer.push(listComputerNum);
     }else{
