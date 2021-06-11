35034e66f0eb37b3f252eab9fa48c6dcc908a972
parix1999 <parisottoedoardo@gmail.com>
2021-06-11 12:38:56 +0200

    js-7.0

--- js/script.js
+++ js/script.js
@@ -26,18 +26,22 @@ numUtente = [];
 /* ora si genera un ciclo while per chiedere all'utente i numeri da inserire nell' array
 e poi per far in modo da non far ripetere il numero selezionato.
 */
-while (numUtente < 84) {
+while (numUtente.length < 5) {
     // inPut utente:
-    var numeroUtente = parseInt(prompt('Inserisci un numero'));
+    var numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 100'));
+    // to do : e se l'utente inserisce un numero > di 100 o minore di 1??
+
     // controllo del numero inserito dell'utente se è all'interno dell'array, e che non sia anche lo stesso generato dal computer:
     if (!numUtente.includes(numeroUtente)) {
         // se il numero dell'utente non c'è nella lista dei numeri generati dal computer allora si continua:
-        if (!numComputer.includes(numeroutente)) {
+        if (!numComputer.includes(numeroUtente)) {
+            numUtente.push(numeroUtente);
             alert('Hai vinto');
         // se il numero è presente nella lista generata dal computer si trova la bomba e quindi si perde: 
         }else{
             alert('Hai perso');
-        }
-        
+        }        
     }
 }
+
+console.log('Numeri utente', numUtente);
